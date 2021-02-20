ebea324a88c61a3b87824e010a9385cc243f326a
naib864 <naib864@naib.top>
2021-02-20 17:52:12 +0100

    Fixed last commit

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -4,18 +4,20 @@
 sudo pacman -Syu --noconfirm
 
 # install regolith
-cd ..
-git clone https://github.com/gardotd426/regolith-de && cd regolith-de
+cd ~/temp
+git clone https://github.com/gardotd426/regolith-de
+cd regolith-de
 makepkg -si
-cd ..
+cd ~/temp
 
 # install rofication for polybar
-git clone https://github.com/naib864/regolith-rofication && cd regolith-rofication
+git clone https://github.com/naib864/regolith-rofication
+cd regolith-rofication
 sudo python3 setup.py install
-cd ..
+cd ~/temp
 
 # install some applications
-for i in gnome-terminal betterlockscreen vlc ncdu mousepad eog bpytop atom fish kdeconnect gucharmap qt5-tools sddm; do
+for i in gnome-terminal vlc ncdu mousepad eog bpytop atom fish kdeconnect gucharmap qt5-tools sddm; do
   sudo pacman -S $i --noconfirm
 done
 
@@ -24,23 +26,31 @@ yay -S ttf-material-design-icons-webfont
 # disable lightdm and set systemd sddm service
 sudo systemctl disable lightdm.service
 sudo systemctl enable sddm.service
-
 sudo pacman -Rs arcolinux-lightdm-gtk-greeter arcolinux-lightdm-gtk-greeter-settings lightdm
 
+# Remove unused applications and reinstall betterlockscreen
+sudo ~/temp/regolith-config/install-scripts/remove-preinstalled.sh
+sudo pacman -S betterlockscreen
+
 # copy configs etc.
-cp -r Pictures/ ~/
-sudo cp -r regolith-config/.config ~/
-sudo cp -r regolith-config/.local ~/
-chmod +x ~/.config/polybar/midnight/*.sh
-chmod +x ~/.config/polybar/midnight/scripts/*.sh
-sudo cp -r etc/ /
-sudo cp -r usr /
-cd ..
+cd ~/temp/regolith-config
+cp -r ~/temp/regolith-config/Pictures/ ~/
+sudo cp -r ~/temp/regolith-config/.config ~/
+sudo cp -r ~/temp/regolith-config/.local ~/
+sudo chmod +x ~/.config/polybar/midnight/*.sh
+sudo chmod +x ~/.config/polybar/midnight/scripts/*.sh
+sudo cp -r ~/temp/regolith-config/etc /
+sudo cp -r ~/temp/regolith-config/usr /
+cd ~/temp
 
 # lock screen setup
 betterlockscreen -u ~/Pictures/itanium/background.jpg
 sudo systemctl enable betterlockscreen@naib864.service
+gsettings set org.gnome.gnome-flashback screensaver false
 
 # spotify polybar
 yay -S polybar-spotify-module
 systemctl --user enable spotify-listener
+
+# apply regolith-look
+regolith-look set midnight

aa7354a4db61aa10513ec0f573a3bf4a6478fdd5
naib864 <naib864@naib.top>
2021-02-20 21:18:30 +0100

    Fixed bugs and changed T61 bar

--- install-scripts/T61.sh
+++ install-scripts/T61.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/bash
 
 # replace module definitions
-mv ~/config/polybar/midnight/modules/network-T61.ini ~/config/polybar/midnight/modules/network.ini
-mv ~/config/polybar/midnight/modules/temperature-T61.ini ~/config/polybar/midnight/modules/temperature.ini
-
-# replace bar definitions
-mv ~/config/polybar/midnight/bars/bar-bottom-T61.ini ~/config/polybar/midnight/bars/bar-bottom.ini
-mv ~/config/polybar/midnight/bars/bar-top-T61.ini ~/config/polybar/midnight/bars/bar-top.ini
+mv ~/.config/polybar/midnight/modules/network-T61.ini ~/.config/polybar/midnight/modules/network.ini
+mv ~/.config/polybar/midnight/modules/temperature-T61.ini ~/.config/polybar/midnight/modules/temperature.ini
 
 # no experimental backends (and blur) for T61
 sudo mv /usr/share/regolith-compositor/init-T61 /usr/share/regolith-compositor/init
--- install-scripts/pc.sh
+++ install-scripts/pc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 # replace module definitions
-mv ~/config/polybar/midnight/modules/network-pc.ini ~/config/polybar/midnight/modules/network.ini
-mv ~/config/polybar/midnight/modules/temperature-pc.ini ~/config/polybar/midnight/modules/temperature.ini
+mv ~/.config/polybar/midnight/modules/network-pc.ini ~/.config/polybar/midnight/modules/network.ini
+mv ~/.config/polybar/midnight/modules/temperature-pc.ini ~/.config/polybar/midnight/modules/temperature.ini
 
 # replace bar definitions
-mv ~/config/polybar/midnight/bars/bar-bottom-pc.ini ~/config/polybar/midnight/bars/bar-bottom.ini
-mv ~/config/polybar/midnight/bars/bar-top-pc.ini ~/config/polybar/midnight/bars/bar-top.ini
+mv ~/.config/polybar/midnight/bars/bar-bottom-pc.ini ~/.config/polybar/midnight/bars/bar-bottom.ini
+mv ~/.config/polybar/midnight/bars/bar-top-pc.ini ~/.config/polybar/midnight/bars/bar-top.ini

98c19e232ea8bd3a5f5259e5684e7ab3dcf73f17
naib864 <naib864@naib.top>
2021-02-20 13:16:33 +0100

    Fixed stuff in install.sh

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -16,7 +16,7 @@ cp -r regolith-config/.local ~/
 chmod +x ~/.config/polybar/midnight/*.sh
 chmod +x ~/.config/polybar/midnight/scripts/*.sh
 sudo cp -r etc/ /
-sudo cp -r usr/ /
+sudo cp -r usr/lib/systemd /usr/lib
 cd ..
 
 # install rofication for polybar
@@ -25,13 +25,14 @@ sudo python3 setup.py install
 cd ..
 
 # install some applications
-for i in gnome-terminal bpytop atom fish kdeconnect gucharmap qt5-tools sddm; do
+for i in gnome-terminal mousepad eog bpytop atom fish kdeconnect gucharmap qt5-tools sddm; do
   sudo pacman -S $i --noconfirm
 done
 
 yay -S ttf-material-design-icons-webfont
 
 # disable lightdm and set systemd sddm service
+sudo cp -r usr/lib/sddm /usr/lib
 sudo systemctl disable lightdm.service
 sudo systemctl enable sddm.service
 
@@ -40,5 +41,5 @@ sudo pacman -Rs arcolinux-lightdm-gtk-greeter-settings
 sudo pacman -Rs lightdm
 
 # lock screen setup
-betterlockscreen -u Pictures/itanium/background.jpg
+betterlockscreen -u ~/Pictures/itanium/background.jpg
 sudo systemctl enable betterlockscreen@naib864.service

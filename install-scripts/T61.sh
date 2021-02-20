ceff623f49b3befe8a35cabdd467c498a1dfade2
naib864 <naib864@naib.top>
2021-02-20 21:19:38 +0100

    Added sudo

--- install-scripts/T61.sh
+++ install-scripts/T61.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 # replace module definitions
-mv ~/.config/polybar/midnight/modules/network-T61.ini ~/.config/polybar/midnight/modules/network.ini
-mv ~/.config/polybar/midnight/modules/temperature-T61.ini ~/.config/polybar/midnight/modules/temperature.ini
+sudo mv ~/.config/polybar/midnight/modules/network-T61.ini ~/.config/polybar/midnight/modules/network.ini
+sudo mv ~/.config/polybar/midnight/modules/temperature-T61.ini ~/.config/polybar/midnight/modules/temperature.ini
 
 # no experimental backends (and blur) for T61
 sudo mv /usr/share/regolith-compositor/init-T61 /usr/share/regolith-compositor/init

7150898ebb8e5065e7be7af1b9b33ccd96a06734
naib864 <naib864@naib.top>
2021-04-05 18:55:11 +0200

    Added tablet mode and On-Screen-Keyboard

--- install-scripts/X1.sh
+++ install-scripts/X1.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
 # some programs
-for i in libreoffice xournalpp discord steam tlp acpi_call tp_smapi; do
+for i in libreoffice xournalpp discord steam tlp acpi_call tp_smapi onboard; do
   sudo pacman -S $i --noconfirm
 done
 
 #enable tlp
 sudo systemctl enable tlp
 sudo systemctl start tlp
+
+# tablet mode
+yay -S detect-tablet-mode-git

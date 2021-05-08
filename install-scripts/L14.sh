8e2e30e3decf9374d094dad204c04b6fd819338f
naib864 <naib864@naib.top>
2021-05-08 16:38:52 +0200

    Added symlinks

--- install-scripts/L14.sh
+++ install-scripts/L14.sh
@@ -14,3 +14,11 @@ sudo systemctl start tlp
 # replace module definitions
 sudo mv ~/.config/polybar/midnight/modules/backlight-L14.ini ~/.config/polybar/midnight/modules/backlight.ini
 sudo mv ~/.config/polybar/midnight/modules/temperature-L14.ini ~/.config/polybar/midnight/modules/temperature.ini
+
+# create symlinks
+sudo mount /dev/mmcblk0 /run/media/naib864/Files
+rm -r ~/Pictures ~/Videos ~/Music ~/Documents
+ln -s /run/media/naib864/Files/Pictures ~
+ln -s /run/media/naib864/Files/Videos ~
+ln -s /run/media/naib864/Files/Music ~
+ln -s /run/media/naib864/Files/Documents ~
